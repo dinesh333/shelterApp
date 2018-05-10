4a648126075591ca0f0d1698f459f355a02a861a
dinesh333 <dinesh123basnet@gmail.com>
2018-05-10 11:45:55 -0400

    testing

--- src/components/common/HomePage.js
+++ src/components/common/HomePage.js
@@ -5,11 +5,13 @@ const Home = () => {
     <div>
       <h1>Home Page</h1>
 
-      <p>Welcome!</p>
+      <p>Welcome!!</p>
 
       <p>Thank you for visiting our website!</p>
 
       <p>Please add whatever a particualr homeless shelter needs!</p>
+
+      <p>...</p>
     </div>
   );
 }
